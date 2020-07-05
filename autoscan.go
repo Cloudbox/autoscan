936b757b3ca46a5699eee95f198fd3e98138a56b
Storm Timmermans <stormtimmermans@icloud.com>
2020-07-05 22:30:26 +0200

    feat(cmd): yaml config (#2)

--- autoscan.go
+++ autoscan.go
@@ -1,6 +1,10 @@
 package autoscan
 
-import "net/http"
+import (
+	"net/http"
+	"path"
+	"strings"
+)
 
 // A Scan is at the core of Autoscan.
 // It defines which path to scan and with which (trigger-given) priority.
@@ -12,9 +16,33 @@ type Scan struct {
 	Metadata Metadata
 }
 
+// Metadata is an optional extension to autoscan.Scan.
+// It defines the provider (e.g. IMDb or TVDb) and the corresponding ID.
+//
+// Metadata MAY be used by targets to get a perfect match.
 type Metadata struct {
 	Provider string
 	ID       string
+}
+
+// BaseConfig defines the base config all Triggers and Targets MUST support.
+type BaseConfig struct {
+	AddPrefix   string `yaml:"addPrefix"`
+	StripPrefix string `yaml:"stripPrefix"`
+}
+
+// TriggerConfig extends the BaseConfig and adds Trigger-specific fields.
+type TriggerConfig struct {
+	BaseConfig `yaml:",inline"`
+	Priority   int `yaml:"priority"`
+}
+
+// HTTPTriggerConfig extends the TriggerConfig and adds HTTP-related fields.
+type HTTPTriggerConfig struct {
+	TriggerConfig `yaml:",inline"`
+
+	// Name MUST be used by the router to separate instances of the same trigger.
+	Name string `yaml:"name"`
 }
 
 // A Trigger pushes new Scans to the given channel.
@@ -37,7 +65,24 @@ type HTTPTrigger func(chan Scan) http.Handler
 type Target func(Scan)
 
 const (
+	// TVDb provider for use in autoscan.Metadata
 	TVDb = "tvdb"
+
+	// TMDb provider for use in autoscan.Metadata
 	TMDb = "tmdb"
+
+	// IMDb provider for use in autoscan.Metadata
 	IMDb = "imdb"
 )
+
+// AddPrefix adds the specified prefix to the given path.
+// If the prefix does not start with the slash, it gets added.
+func AddPrefix(prefix string, p string) string {
+	return path.Join("/", prefix, p)
+}
+
+// StripPrefix removes the specified prefix from the given path.
+// A slash at the end of the prefix will be ignored.
+func StripPrefix(prefix string, p string) string {
+	return path.Join("/", strings.TrimPrefix(p, prefix))
+}
--- triggers/radarr/radarr.go
+++ triggers/radarr/radarr.go
@@ -10,16 +10,19 @@ import (
 )
 
 // New creates an autoscan-compatible HTTP Trigger for Radarr webhooks.
-//
-// TODO: New should accept a Radarr-specific path rewriter.
-func New() autoscan.HTTPTrigger {
+func New(c Config) autoscan.HTTPTrigger {
 	return func(scans chan autoscan.Scan) http.Handler {
-		return &handler{scans: scans}
+		return &handler{config: c, scans: scans}
 	}
 }
 
+type Config struct {
+	autoscan.HTTPTriggerConfig `yaml:",inline"`
+}
+
 type handler struct {
-	scans chan autoscan.Scan
+	config Config
+	scans  chan autoscan.Scan
 }
 
 type radarrEvent struct {
@@ -61,10 +64,19 @@ func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: Rewrite the path based on the path-rewriter in the handler struct.
+	// Rewrite the path based on the provided AddPrefix and StripPrefix values.
+	filePath := path.Join(event.Movie.FolderPath, event.File.RelativePath)
+	if h.config.StripPrefix != "" {
+		filePath = autoscan.StripPrefix(h.config.StripPrefix, filePath)
+	}
+
+	if h.config.AddPrefix != "" {
+		filePath = autoscan.AddPrefix(h.config.AddPrefix, filePath)
+	}
+
 	scan := autoscan.Scan{
-		Path:     path.Join(event.Movie.FolderPath, event.File.RelativePath),
-		Priority: 1,
+		Path:     filePath,
+		Priority: h.config.Priority,
 	}
 
 	if event.Details.ImdbID != "" {

14ee8a26ecc4f4376fdddee1cd3fde6bc946bff7
l3uddz <l3uddz@gmail.com>
2020-08-02 21:28:58 +0100

    refactor(targets): switch to emby api (#24)
    Drops Emby's SQLite database in favour of using the API for all operations.

---
+++ targets/emby/api.go
@@ -0,0 +1,173 @@
+package emby
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/cloudbox/autoscan"
+	"github.com/rs/zerolog"
+	"net/http"
+)
+
+type apiClient struct {
+	url   string
+	token string
+	log   zerolog.Logger
+}
+
+func newApiClient(c Config) *apiClient {
+	return &apiClient{
+		url:   c.URL,
+		token: c.Token,
+		log: autoscan.GetLogger(c.Verbosity).With().
+			Str("target", "emby").
+			Str("url", c.URL).
+			Logger(),
+	}
+}
+
+func (c apiClient) Available() error {
+	// create request
+	req, err := http.NewRequest("GET", autoscan.JoinURL(c.url, "emby", "System", "Info"), nil)
+	if err != nil {
+		return fmt.Errorf("failed creating availability request: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	// set headers
+	req.Header.Set("X-Emby-Token", c.token)
+	req.Header.Set("Accept", "application/json")
+
+	// send request
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed sending availability request: %v: %w", err, autoscan.ErrTargetUnavailable)
+	}
+
+	defer res.Body.Close()
+
+	// validate response
+	if res.StatusCode != 200 {
+		return fmt.Errorf("%v: failed validating availability request response: %w",
+			res.Status, autoscan.ErrTargetUnavailable)
+	}
+
+	return nil
+}
+
+type library struct {
+	ID   string
+	Name string
+	Path string
+}
+
+func (c apiClient) Libraries() ([]library, error) {
+	// create request
+	reqURL := autoscan.JoinURL(c.url, "emby", "Library", "SelectableMediaFolders")
+	req, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating libraries request: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	// set headers
+	req.Header.Set("X-Emby-Token", c.token)
+	req.Header.Set("Accept", "application/json")
+
+	// send request
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed sending libraries request: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	defer res.Body.Close()
+
+	// validate response
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("%v: failed validating libraries request response: %w",
+			res.Status, autoscan.ErrFatal)
+	}
+
+	// decode response
+	type Response struct {
+		Name    string `json:"Name"`
+		Folders []struct {
+			Path string `json:"Path"`
+		} `json:"SubFolders"`
+	}
+
+	resp := make([]Response, 0)
+	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
+		return nil, fmt.Errorf("failed decoding libraries request response: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	// process response
+	libraries := make([]library, 0)
+	for _, lib := range resp {
+		for _, folder := range lib.Folders {
+			libraries = append(libraries, library{
+				Name: lib.Name,
+				Path: folder.Path,
+			})
+		}
+	}
+
+	return libraries, nil
+}
+
+type scanRequest struct {
+	Path       string `json:"path"`
+	UpdateType string `json:"updateType"`
+}
+
+func (c apiClient) Scan(path string) error {
+	// create request payload
+	type Payload struct {
+		Updates []scanRequest `json:"Updates"`
+	}
+
+	payload := &Payload{
+		Updates: []scanRequest{
+			{
+				Path:       path,
+				UpdateType: "Created",
+			},
+		},
+	}
+
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed encoding scan request payload: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	// create request
+	reqURL := autoscan.JoinURL(c.url, "Library", "Media", "Updated")
+	req, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(b))
+	if err != nil {
+		// May only occur when the user has provided an invalid URL
+		return fmt.Errorf("failed creating scan request: %v: %w", err, autoscan.ErrFatal)
+	}
+
+	// set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Emby-Token", c.token)
+
+	// send request
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed sending scan request: %v: %w", err, autoscan.ErrTargetUnavailable)
+	}
+
+	defer res.Body.Close()
+
+	// validate response
+	switch res.StatusCode {
+	case 204:
+		// success
+		return nil
+	case 401:
+		// unauthorized
+		return fmt.Errorf("emby token is invalid: failed validating scan request response: %w", autoscan.ErrFatal)
+	}
+
+	return fmt.Errorf("%v: failed validating scan request response: %w",
+		res.Status, autoscan.ErrTargetUnavailable)
+}
--- targets/emby/scan.go
+++ targets/emby/scan.go
@@ -1,22 +1,11 @@
 package emby
 
 import (
-	"bytes"
-	"database/sql"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
-	"path/filepath"
 	"strings"
 
 	"github.com/cloudbox/autoscan"
 )
-
-type scanRequest struct {
-	Path       string `json:"path"`
-	UpdateType string `json:"updateType"`
-}
 
 func (t target) Scan(scans []autoscan.Scan) error {
 	// ensure scan tasks present (should never fail)
@@ -24,60 +13,19 @@ func (t target) Scan(scans []autoscan.Scan) error {
 		return nil
 	}
 
-	// check for at-least one missing/changed file
-	process := false
-	for _, s := range scans {
-		targetFilePath := t.rewrite(filepath.Join(s.Folder, s.File))
-
-		targetFile, err := t.store.MediaPartByFile(targetFilePath)
-		if err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				// local file not found in target
-				t.log.Debug().
-					Str("path", targetFilePath).
-					Msg("At least one local file did not exist in target datastore")
-
-				process = true
-				break
-			}
-
-			// unexpected error
-			return fmt.Errorf("could not check emby datastore: %v: %w", err, autoscan.ErrFatal)
-		}
-
-		// local file was found in target
-		if targetFile.Size != s.Size {
-			// local file did not match in target
-			t.log.Debug().
-				Str("path", targetFilePath).
-				Uint64("target_size", targetFile.Size).
-				Uint64("local_size", s.Size).
-				Msg("Local file size does not match in target datastore")
-
-			process = true
-			break
-		}
-	}
-
-	if !process {
-		// all local files existed in target
-		t.log.Debug().
-			Interface("scans", scans).
-			Msg("All local files exist in target")
-		return nil
-	}
-
+	// determine library for this scan
 	scanFolder := t.rewrite(scans[0].Folder)
 
-	// determine library for this scan
 	lib, err := t.getScanLibrary(scanFolder)
 	if err != nil {
 		t.log.Warn().
 			Err(err).
+			Str("path", scanFolder).
 			Msg("No target library found")
-		return fmt.Errorf("%v: %w", err, autoscan.ErrRetryScan)
+		return fmt.Errorf("%v: target library not found: %v: %w", scanFolder, err, autoscan.ErrRetryScan)
 	}
 
+	// send scan request
 	l := t.log.With().
 		Str("path", scanFolder).
 		Str("library", lib.Name).
@@ -85,45 +33,8 @@ func (t target) Scan(scans []autoscan.Scan) error {
 
 	l.Trace().Msg("Sending scan request")
 
-	// create request payload
-	payload := new(struct {
-		Updates []scanRequest `json:"Updates"`
-	})
-
-	payload.Updates = append(payload.Updates, scanRequest{
-		Path:       scanFolder,
-		UpdateType: "Created",
-	})
-
-	b, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("failed encoding scan request payload: %v, %w", err, autoscan.ErrFatal)
-	}
-
-	// create request
-	reqURL := autoscan.JoinURL(t.url, "Library", "Media", "Updated")
-	req, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(b))
-	if err != nil {
-		// May only occur when the user has provided an invalid URL
-		return fmt.Errorf("failed creating scan request: %v: %w", err, autoscan.ErrFatal)
-	}
-
-	// set headers
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-Emby-Token", t.token)
-
-	// send request
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed sending scan request: %v: %w", err, autoscan.ErrTargetUnavailable)
-	}
-
-	defer res.Body.Close()
-
-	// validate response
-	if res.StatusCode != 204 {
-		// 404 if some kind of proxy is in-front of Emby while it's offline.
-		return fmt.Errorf("%v: failed validating scan request response: %w", res.Status, autoscan.ErrTargetUnavailable)
+	if err := t.api.Scan(scanFolder); err != nil {
+		return err
 	}
 
 	l.Info().Msg("Scan moved to target")

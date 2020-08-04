a6bb2f33119f869bb532f85f878b6c880bba9942
Storm Timmermans <stormtimmermans@icloud.com>
2020-08-04 12:24:20 +0200

    feat: non-working bernard

--- go.mod
+++ go.mod
@@ -7,9 +7,12 @@ require (
 	github.com/alecthomas/kong v0.2.11
 	github.com/justinas/alice v1.2.0
 	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
-	github.com/mattn/go-sqlite3 v1.14.0
+	github.com/m-rots/bernard v0.3.2
+	github.com/m-rots/stubbs v1.0.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/rs/zerolog v1.19.0
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c
---
+++ triggers/bernard/bernard.go
@@ -0,0 +1,170 @@
+package bernard
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/cloudbox/autoscan"
+	lowe "github.com/m-rots/bernard"
+	ds "github.com/m-rots/bernard/datastore"
+	"github.com/m-rots/bernard/datastore/sqlite"
+	"github.com/m-rots/stubbs"
+	"github.com/robfig/cron/v3"
+	"github.com/rs/zerolog"
+)
+
+type Config struct {
+	CronSchedule  string `yaml:"cron"`
+	AccountPath   string `yaml:"account"`
+	DatastorePath string `yaml:"database"`
+	DriveID       string `yaml:"id"`
+	Verbosity     string `yaml:"verbosity"`
+}
+
+func New(c Config) (autoscan.Trigger, error) {
+	l := autoscan.GetLogger(c.Verbosity).With().
+		Str("trigger", "bernard").
+		Str("drive_id", c.DriveID).
+		Logger()
+
+	auth, err := stubbsFromFile(c.AccountPath)
+	if err != nil {
+		return nil, fmt.Errorf("%v: %w", err, autoscan.ErrFatal)
+	}
+
+	store, err := sqlite.New(c.DatastorePath)
+	if err != nil {
+		return nil, fmt.Errorf("%v: %w", err, autoscan.ErrFatal)
+	}
+
+	bernard := lowe.New(auth, store)
+
+	trigger := func(callback autoscan.ProcessorFunc) {
+		d := daemon{
+			log:          l,
+			callback:     callback,
+			cronSchedule: c.CronSchedule,
+			driveID:      c.DriveID,
+			bernard:      bernard,
+			store:        store,
+		}
+
+		if err := d.InitialSync(); err != nil {
+			l.Error().Err(err).Msg("Initial sync failed")
+			return
+		}
+
+		// start job
+		if err := d.StartAutoSync(); err != nil {
+			l.Error().Err(err).Msg("Cron job could not be created")
+			return
+		}
+	}
+
+	return trigger, nil
+}
+
+type daemon struct {
+	callback     autoscan.ProcessorFunc
+	cronSchedule string
+	driveID      string
+	bernard      *lowe.Bernard
+	store        *sqlite.Datastore
+	log          zerolog.Logger
+}
+
+func (d daemon) InitialSync() error {
+	_, err := d.store.PageToken(d.driveID)
+	switch {
+	case err == nil:
+		return nil
+	case errors.Is(err, ds.ErrFullSync):
+		d.log.Info().Msg("Starting full sync")
+		if err := d.bernard.FullSync(d.driveID); err != nil {
+			return err
+		}
+		d.log.Info().Msg("Finished full sync")
+	default:
+		return err
+	}
+
+	return nil
+}
+
+type syncJob struct {
+	job       func()
+	mtx       sync.Mutex
+	isRunning bool
+}
+
+func (s *syncJob) Do() {
+	if s.isRunning {
+		return
+	}
+
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
+	s.isRunning = true
+	s.job()
+	s.isRunning = false
+}
+
+func newSyncJob(job func()) *syncJob {
+	return &syncJob{
+		job: job,
+	}
+}
+
+func (d daemon) StartAutoSync() error {
+	c := cron.New()
+
+	job := newSyncJob(func() {
+		d.log.Trace().Msg("Running partial sync")
+		err := d.bernard.PartialSync(d.driveID)
+		if err != nil {
+			d.log.Error().Err(err).Msg("Partial sync failed")
+			c.Stop()
+			return
+		}
+		d.log.Trace().Msg("Partial sync complete")
+	})
+
+	_, err := c.AddFunc(d.cronSchedule, job.Do)
+	if err != nil {
+		return err
+	}
+
+	c.Start()
+	return nil
+}
+
+func stubbsFromFile(filename string) (*stubbs.Stubbs, error) {
+	const scope string = "https://www.googleapis.com/auth/drive.readonly"
+
+	type serviceAccount struct {
+		Email string `json:"client_email"`
+		Key   string `json:"private_key"`
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	sa := new(serviceAccount)
+	err = json.NewDecoder(file).Decode(sa)
+	if err != nil {
+		return nil, err
+	}
+
+	priv, err := stubbs.ParseKey(sa.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	return stubbs.New(sa.Email, &priv, []string{scope}, 3600), nil
+}
